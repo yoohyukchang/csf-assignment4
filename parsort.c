28a33ab2eb7f56f3cadb4bac4c9f700bbb921378
Yoohyuk Chang <yoohyukchang@gmail.com>
2023-11-08 12:33:23 -0500

    updated parsort

--- parsort.c
+++ parsort.c
@@ -70,9 +70,64 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
 
   size_t mid = begin + size/2;
 
-  // TODO: parallelize the recursive sorting
-  merge_sort(arr, begin, mid, threshold);
-  merge_sort(arr, mid, end, threshold);
+  // parallelize the recursive sorting
+  // sort left half in a new process
+  pid_t left_pid = fork();
+  if (left_pid == -1) {
+    fprintf(stderr, "Error: left fork child pid failed to start a new process!");
+    exit(1);
+  } else if (left_pid == 0) {
+    merge_sort(arr, begin, mid, threshold);
+    exit(0);
+  }
+
+  // sort right half in a new process
+  pid_t right_pid = fork();
+  if (right_pid == -1) {
+    fprintf(stderr, "Error: right fork child pid failed to start a new process!");
+    exit(1);
+  } else if (right_pid == 0) {
+    merge_sort(arr, mid, end, threshold);
+    exit(0);
+  }
+
+  // blocks until the process indentified by pid_to_wait_for completes (left_pid)
+  int wstatus_left;
+  pid_t actual_pid_left = waitpid(left_pid, &wstatus_left, 0);
+
+  if (actual_pid_left == -1) {
+    fprintf(stderr, "Error: waitpid failure! (left)");
+    exit(1);
+  }
+
+  if (!WIFEXITED(wstatus_left)) {
+    fprintf(stderr, "Error: subprocess crashed, was interruped, or did not exit normally! (left)");
+    exit(1);
+  }
+  if (WEXITSTATUS(wstatus_left) != 0) {
+    fprintf(stderr, "Error: subprocess returned a non-zero exit code! (left)");
+    exit(1);
+  }
+
+  // blocks until the process indentified by pid_to_wait_for completes (right_pid)
+  int wstatus_right;
+  int actual_pid_right = waitpid(right_pid, &wstatus_right, 0);
+
+  if (actual_pid_right == -1) {
+    fprintf(stderr, "Error: waitpid failure! (right)");
+    exit(1);
+  }
+
+  if (!WIFEXITED(wstatus_right)) {
+    fprintf(stderr, "Error: subprocess crashed, was interruped, or did not exit normally! (right)");
+    exit(1);
+  }
+
+  if (WEXITSTATUS(wstatus_right) != 0) {
+    fprintf(stderr, "Error: subprocess returned a non-zero exit code! (right)");
+    exit(1);
+  }  
+
 
   // allocate temp array now, so we can avoid unnecessary work
   // if the malloc fails
@@ -106,18 +161,53 @@ int main(int argc, char **argv) {
   char *end;
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
   if (end != argv[2] + strlen(argv[2])) {
-    // TODO: report an error (threshold value is invalid)
+    // report an error (threshold value is invalid)
+    fprintf(stderr, "Error: Invalid threshold value!");
+    return 1;
   }
 
-  // TODO: open the file
+  // open the file
+  int fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    // file couldn't be opened: handle error and exit
+    fprintf(stderr, "Error: file could not be opened!");
+    return 1;
+  }
 
-  // TODO: use fstat to determine the size of the file
+  // use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+    // handle fstat error and exit
+    fprintf(stderr, "Error: fstat error!");
+    return 1;
+  }
+  size_t file_size_in_bytes = statbuf.st_size;
 
-  // TODO: map the file into memory using mmap
+  // map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  // Immediately close the file descriptor as it's no longer needed
+  int close_status = close(fd);
+  if (close_status != 0) {
+    fprintf(stderr, "Error: file cannot be closed properly!");
+    return 1;
+  }
+  // handle mmap error and exit
+  if (data == MAP_FAILED) {
+    fprintf(stderr, "Error: Could not map the into the memory!");
+    return 1;
+  }
 
-  // TODO: sort the data!
+  // sort the data
+  merge_sort(data, 0, file_size_in_bytes / sizeof(int64_t), threshold);
 
-  // TODO: unmap and close the file
+  // unmap and close the file
+  int munmap_status = munmap(data, file_size_in_bytes);
+  if (munmap_status == -1) {
+    fprintf(stderr, "Error: Could not unmap the memory!");
+    return 1;
+  }
 
-  // TODO: exit with a 0 exit code if sort was successful
+  // exit with a 0 exit code if sort was successful
+  return 0;
 }
